d50f6dbbabb332bf062c077d121de8b02244a417
Minsung Ryu <ryums0227@gmail.com>
2016-10-19 17:43:54 +0900

    Initial Commit

---
+++ HeaderWithPagingMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HeaderWithPagingMenu
+//
+//  Created by lastam99 on 2016. 10. 19..
+//  Copyright © 2016년 lastam99. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
