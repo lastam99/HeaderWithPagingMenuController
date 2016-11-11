11ba2a69e80f81ea415650d691f57a50dc758adf
이규진 <amanida@stone-i.co.kr>
2016-11-11 15:19:37 +0900

    implemented headerBehavior in HeaderConstraintDelegate. fixed build error

--- PagingMenuWithHeader/SomeTableViewController.swift
+++ PagingMenuWithHeader/SomeTableViewController.swift
@@ -8,15 +8,13 @@
 
 import UIKit
 
-let YOUR_HEADER_SIZE: CGFloat = 100
-
 class SomeTableViewController: UITableViewController {
 
 	weak var headerConstraintDelegate: HeaderConstraintDelegate?
 	
-	var offset: CGFloat = 0							// contentsOffset of table view
+	var offset: CGFloat = 0				// contentsOffset of table view
 	var allowTableScroll: Bool = false	// before header is being unvisible, table view scroll will be disabled
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		self.tableView.showsVerticalScrollIndicator = false
@@ -51,33 +49,7 @@ class SomeTableViewController: UITableViewController {
 	}
     
 	override func scrollViewDidScroll(_ scrollView: UIScrollView) {
-		let constraint: CGFloat = (headerConstraintDelegate?.currentHeaderConstraint())!
-		
-																															// header visible, scroll down -> case 3
-		if -YOUR_HEADER_SIZE < constraint {												// header visible, scroll down -> case 2
-			offset = -constraint + scrollView.contentOffset.y				// current header constraint + table view offset
-			allowTableScroll = false
-		} else {																									// header unvisible
-			offset = YOUR_HEADER_SIZE + scrollView.contentOffset.y	// scroll up -> case 1, scroll down -> case 2
-			allowTableScroll = true
-		}
-        
-		if offset >= YOUR_HEADER_SIZE {														// case 1
-			offset = -YOUR_HEADER_SIZE
-		} else if 0 <= offset && offset < YOUR_HEADER_SIZE {			// case 2
-			offset = -offset
-		} else {																									// case 3
-			offset = 0																							// TODO: bounce animation
-		}
-        
-		headerConstraintDelegate?.updateHeaderConstraint(offset, animate: false)
-		
-		if !allowTableScroll {
-			scrollView.delegate = nil				// prevent callback - maybe not good
-			scrollView.contentOffset.y = 0  // this is trigger
-			scrollView.delegate = self			// reallocate
-		}
-
+        headerConstraintDelegate?.headerBehavior(scrollView: scrollView, viewController: self)
 	}
     
 }
--- PagingMenuWithHeader/ViewController.swift
+++ PagingMenuWithHeader/ViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 import PagingMenuController
 
+// MARK - View Controller
+
 class ViewController: UIViewController {
 	
 	@IBOutlet weak var profile: UILabel!
 	@IBOutlet weak var container: UIView!
-	@IBOutlet weak var headerConstraint: NSLayoutConstraint!
-	
+    @IBOutlet weak var headerConstraint: NSLayoutConstraint!
+    @IBOutlet weak var headerView: UILabel!
+    
 	private var table1: SomeTableViewController? = SomeTableViewController()
-	private var table2: SomeTableViewController? = SomeTableViewController()
+    private var table2: SomeTableViewController? = SomeTableViewController()
+    private var table3: SomeTableViewController? = SomeTableViewController()
+
 	
 	var headerOffset: CGFloat = 0
 	
@@ -25,6 +30,7 @@ class ViewController: UIViewController {
 		
 		table1?.headerConstraintDelegate = self
 		table2?.headerConstraintDelegate = self
+        table3?.headerConstraintDelegate = self
 		
 		setupFramework()
 	}
@@ -38,19 +44,24 @@ class ViewController: UIViewController {
 	func setupFramework() {
 		struct MenuItem1: MenuItemViewCustomizable {}
 		struct MenuItem2: MenuItemViewCustomizable {}
+        struct MenuItem3: MenuItemViewCustomizable {}
 		
 		struct MenuOptions: MenuViewCustomizable {
 			var itemsOptions: [MenuItemViewCustomizable] {
-				return [MenuItem1(), MenuItem2()]
+				return [MenuItem1(), MenuItem2(), MenuItem3()]
 			}
+            var displayMode: MenuDisplayMode {
+                return .segmentedControl
+            }
 		}
 		
 		struct PagingMenuOptions: PagingMenuControllerCustomizable {
 			var custom1: UIViewController?
 			var custom2: UIViewController?
+            var custom3: UIViewController?
 			
 			var componentType: ComponentType {
-				return .all(menuOptions: MenuOptions(), pagingControllers: [custom1!, custom2!])
+				return .all(menuOptions: MenuOptions(), pagingControllers: [custom1!, custom2!, custom3!])
 			}
 			
 			mutating func setComp1(vc: UIViewController?) {
@@ -60,11 +71,16 @@ class ViewController: UIViewController {
 			mutating func setComp2(vc: UIViewController?) {
 				custom2 = vc
 			}
+            
+            mutating func setComp3(vc: UIViewController?) {
+                custom3 = vc
+            }
 		}
 		
 		var options = PagingMenuOptions()
 		options.setComp1(vc: self.table1)
 		options.setComp2(vc: self.table2)
+        options.setComp3(vc: self.table3)
 		
 		let pagingMenuController = self.childViewControllers.first as! PagingMenuController
 		pagingMenuController.delegate = self
@@ -78,25 +94,44 @@ class ViewController: UIViewController {
 extension ViewController: PagingMenuControllerDelegate {
 	
 	func didMove(toMenu menuController: UIViewController, fromMenu previousMenuController: UIViewController) {
-		let from = previousMenuController as! SomeTableViewController
-		
-		updateHeaderConstraint(headerOffset, animate: true)
-		headerOffset = from.currentOffset()
+        
+        // disabled header resizing when menu item changed
+//		let from = previousMenuController as! SomeTableViewController
+//		
+//		updateHeaderConstraint(headerOffset, animate: true)
+//		headerOffset = from.currentOffset()
 	}
-	
 }
 
 // MARK: - Header Constraint Delegate
 
 protocol HeaderConstraintDelegate: class {
-	
-	func updateHeaderConstraint(_ constant: CGFloat, animate: Bool)
-	func currentHeaderConstraint() -> CGFloat
-	
+    var isHeaderVisible: Bool { get }
+    var headerHeight: CGFloat { get }
+    var statusBarHeight: CGFloat { get }
+    var currentHeaderConstraint: CGFloat { get }
+    func updateHeaderConstraint(_ constant: CGFloat, animate: Bool)
+    func headerBehavior(scrollView: UIScrollView, viewController: UITableViewController)
 }
 
 extension ViewController: HeaderConstraintDelegate {
-	
+    var isHeaderVisible: Bool {
+        // if header is visible, Don't allow table scroll
+        return currentHeaderConstraint > statusBarHeight - headerHeight
+    }
+    
+    var headerHeight: CGFloat {
+        return self.headerView.frame.size.height + UIApplication.shared.statusBarFrame.height
+    }
+    
+    var statusBarHeight: CGFloat {
+        return UIApplication.shared.statusBarFrame.height
+    }
+    
+    var currentHeaderConstraint: CGFloat {
+        return self.headerConstraint.constant + statusBarHeight
+    }
+
 	func updateHeaderConstraint(_ constant: CGFloat, animate: Bool) {
 		self.headerConstraint.constant = constant
 		if animate {
@@ -105,9 +140,41 @@ extension ViewController: HeaderConstraintDelegate {
 			})
 		}
 	}
-	
-	func currentHeaderConstraint() -> CGFloat {
-		return headerConstraint.constant
-	}
-	
+
+    func headerBehavior(scrollView: UIScrollView, viewController: UITableViewController) {
+        let scrollViewOffset = scrollView.contentOffset.y
+        let headerVisible: Bool = isHeaderVisible
+        var offset: CGFloat
+        
+        // scroll down -> case 3
+        if headerVisible {
+            // scroll down -> case 2
+            offset = scrollViewOffset - currentHeaderConstraint
+        } else {
+            
+            // scroll up -> case 1, scroll down -> case 2
+            offset = scrollViewOffset + headerHeight
+        }
+        
+        if offset >= headerHeight {
+            // case 1
+            offset = statusBarHeight - headerHeight
+        } else if 0 <= offset && offset < headerHeight {
+            // case 2
+            offset = -offset
+        } else {
+            // case 3
+            offset = 0
+            // TODO: - add bounce animation
+        }
+        
+        self.updateHeaderConstraint(offset - statusBarHeight, animate: false)
+        
+        if headerVisible {
+            scrollView.delegate = nil                 // prevent callback - maybe not good
+            scrollView.contentOffset.y = 0            // this is trigger
+            scrollView.delegate = viewController     // reallocate
+        }
+    }
+
 }
